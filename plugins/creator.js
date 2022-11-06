3bae11607deeb5ef44d9fc3004bb091e67767560
kINGDEVIL01 <117563454+kINGDEVIL01@users.noreply.github.com>
2022-11-06 12:53:47 +0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   
   const kontak = {
-	"displayName": 'My Owner Umar',
+	"displayName": 'My Owner Sameer',
 	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('923023943131@s.whatsapp.net')}\nitem1.TEL;waid=923023943131:923023943131\nitem1.X-ABLabel:save-me.\nURL;My Web: https://github.com/KING DEVIL01\nEMAIL;Email Owner:DevilSameer345@gamil.com\nORG: NOT A BOT +SAVE ME\nTEL;My number ;waid=48888781888:48888781888\nEND:VCARD`
 }
 
